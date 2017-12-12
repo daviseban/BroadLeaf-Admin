c6fcb57c9b88d1c4996c0339902bacad2c8dd826
daviseban <daviseban@gmail.com>
2017-12-12 16:58:02 +0530

    Update AdminApplication.java

--- admin/src/main/java/com/community/admin/AdminApplication.java
+++ admin/src/main/java/com/community/admin/AdminApplication.java
@@ -2,6 +2,8 @@ package com.community.admin;
 
 import org.broadleafcommerce.common.config.EnableBroadleafAdminAutoConfiguration;
 import org.broadleafcommerce.common.web.boot.support.BroadleafBootServletContextInitializer;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
